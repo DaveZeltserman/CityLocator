c045ddaae3d01d6065bacffcec7717d206c87bfb
Dave Zeltserman <dave.zeltserman@gmail.com>
2019-05-04 20:34:08 -0400

    Add comment to test GiKraken

--- src/main/java/com/davez/citylocator/CitylocatorApplication.java
+++ src/main/java/com/davez/citylocator/CitylocatorApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CitylocatorApplication {
 
+	// stamdard SpringBoot main routine
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CitylocatorApplication.class, args);
 	}
