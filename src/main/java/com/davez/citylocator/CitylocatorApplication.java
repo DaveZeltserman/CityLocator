1d67f1e8cf3e17ab6caaab969e06d473f6990960
Dave Zeltserman <dave.zeltserman@gmail.com>
2019-05-04 21:13:24 -0400

    fixed comment

--- src/main/java/com/davez/citylocator/CitylocatorApplication.java
+++ src/main/java/com/davez/citylocator/CitylocatorApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CitylocatorApplication {
 
-	// stamdard SpringBoot main routine
+	// standard SpringBoot main routine
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CitylocatorApplication.class, args);
